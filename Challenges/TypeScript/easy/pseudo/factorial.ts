11e619688018dec3f43bcd1f963360dd82718faa
Amit Bar-Gil <amit217@yandex.com>
2022-09-19 13:42:11 +0300

    Find factorial number

---
+++ Challenges/TypeScript/easy/pseudo/factorial.ts
@@ -0,0 +1,8 @@
+export {};
+//Find the factorial of a given number
+
+function factorial(num) {
+    // 1. If the number is 1 or smaller, return it.
+    // 2. If the number is greater than 1 continue to the next step
+    // 3. Take the number minus one of himself and power it by the number itself
+}
